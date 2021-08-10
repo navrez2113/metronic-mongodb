6c3e678b05bc7c888bf0ca4f0014f02a8792ffd9
navrez2113 <80328072+navrez2113@users.noreply.github.com>
2021-08-10 10:15:27 +0300

    Update MetronicMongodbApplication.java

--- src/main/java/com/gnsofttr/metronicmongodb/MetronicMongodbApplication.java
+++ src/main/java/com/gnsofttr/metronicmongodb/MetronicMongodbApplication.java
@@ -18,25 +18,25 @@ public class MetronicMongodbApplication {
 		SpringApplication.run(MetronicMongodbApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(RoleRepository roleRepository) {
-
-	    return args -> {
-
-	        Role adminRole = roleRepository.findByRole("ADMIN");
-	        if (adminRole == null) {
-	        	Role newAdminRole = new Role();
-	            newAdminRole.setRole("ADMIN");
-	            roleRepository.save(newAdminRole);
-	        }
-
-	        Role userRole = roleRepository.findByRole("USER");
-	        if (userRole == null) {
-	        	Role newUserRole = new Role();
-	            newUserRole.setRole("USER");
-	            roleRepository.save(newUserRole);
-	        }
-	    };
-
-	}
+//	@Bean
+//	CommandLineRunner init(RoleRepository roleRepository) {
+//
+//	    return args -> {
+//
+//	        Role adminRole = roleRepository.findByRole("ADMIN");
+//	        if (adminRole == null) {
+//	        	Role newAdminRole = new Role();
+//	            newAdminRole.setRole("ADMIN");
+//	            roleRepository.save(newAdminRole);
+//	        }
+//
+//	        Role userRole = roleRepository.findByRole("USER");
+//	        if (userRole == null) {
+//	        	Role newUserRole = new Role();
+//	            newUserRole.setRole("USER");
+//	            roleRepository.save(newUserRole);
+//	        }
+//	    };
+//
+//	}
 }
